a8f91bb8003b394002bc02039df0f4deb52fe82d
Gertjan Goetynck <gertjan@goetynck.com>
2021-03-31 09:54:50 +0200

    Added authorization tests

--- src/main/java/com/switchfully/eurder/service/AuthorizationService.java
+++ src/main/java/com/switchfully/eurder/service/AuthorizationService.java
@@ -6,6 +6,8 @@ import com.switchfully.eurder.domain.user.UserRole;
 import com.switchfully.eurder.infrastructure.utils.ValidationUtil;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class AuthorizationService {
     private final UserRepository userRepository;
@@ -15,26 +17,26 @@ public class AuthorizationService {
     }
 
     public void throwExceptionIfNotAdmin(String id) throws IllegalAccessException {
-        if (!isAdmin(id)) {
+        if (!ValidationUtil.isValidUUID(id) || !isAdmin(id)) {
             throw new IllegalAccessException("Only admins can visit this page");
         }
 
     }
 
     public void throwExceptionIfNotMember(String id) throws IllegalAccessException {
-        if (!isMember(id)) {
+        if (!ValidationUtil.isValidUUID(id) || !isMember(id)) {
             throw new IllegalAccessException("Only registered users can visit this page");
         }
 
     }
 
     private boolean isAdmin(String id) {
-        User user = userRepository.getUserById(ValidationUtil.convertStringToUUID(id));
+        User user = userRepository.getUserById(UUID.fromString(id));
         return user != null && user.getUserRole() == UserRole.ADMIN;
     }
 
     private boolean isMember(String id) {
-        User user = userRepository.getUserById(ValidationUtil.convertStringToUUID(id));
+        User user = userRepository.getUserById(UUID.fromString(id));
         return user != null;
     }
 }
--- src/test/java/com/switchfully/eurder/service/AuthorizationServiceTest.java
+++ src/test/java/com/switchfully/eurder/service/AuthorizationServiceTest.java
@@ -1,5 +1,6 @@
 package com.switchfully.eurder.service;
 
+import com.switchfully.eurder.domain.user.Customer;
 import com.switchfully.eurder.domain.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -31,6 +32,32 @@ class AuthorizationServiceTest {
     @Test
     void givingCorrectId_whenValidatingAdmin_thenLogIn() {
         Executable executable = () -> authorizationService.throwExceptionIfNotAdmin("123e4567-e89b-12d3-a456-556642440000");
+        assertDoesNotThrow(executable);
+    }
+
+    @Test
+    void givingNullId_whenValidatingMember_thenThrowIllegalAccessException() {
+        Executable executable = () -> authorizationService.throwExceptionIfNotMember(null);
+        assertThrows(IllegalAccessException.class, executable);
+    }
+
+    @Test
+    void givingWrongSizedId_whenValidatingMember_thenThrowIllegalAccessException() {
+        Executable executable = () -> authorizationService.throwExceptionIfNotMember("karel");
+        assertThrows(IllegalAccessException.class, executable);
+    }
+
+    @Test
+    void givingWrongId_whenValidatingMember_thenThrowIllegalAccessException() {
+        Executable executable = () -> authorizationService.throwExceptionIfNotMember("123e4567-e89b-12d3-a456-556642440007");
+        assertThrows(IllegalAccessException.class, executable);
+    }
+
+    @Test
+    void givingCorrectId_whenValidatingMember_thenLogIn() {
+        Customer customer = new Customer("Test", "Test", "test@dummy.com", "test", "test");
+        userRepository.addUser(customer);
+        Executable executable = () -> authorizationService.throwExceptionIfNotMember(customer.getId().toString());
         assertDoesNotThrow(executable);
     }
 }
